9566a831eb6aed4a4d175552551beaa2ef9e8012
katiedemp <KatieDempsey@Katies-MacBook-2.local>
2016-11-03 20:37:12 +1300

    posting photos

--- index.js
+++ index.js
@@ -23,12 +23,15 @@ app.get('/posts', sendPostsList);
 //let a client POST something new
 var saveNewPost = function (request, response) {
   console.log(request.body.message); //write it on the command prompt so we can see
+  console.log(request.body.photo);
   var post= {};
 post.message = request.body.message;
+post.photo = request.body.photo;
 posts.push(post);
   response.send("thanks for your message. Press back to add another");
 }
 app.post('/posts', saveNewPost);
+
 
 //listen for connections on port 3000
 app.listen(3000);
